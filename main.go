4432d86e94af14316a5ac8806bda3726152ced8a
eastonsmith <eastonsmith37@gmail.com>
2022-03-14 12:09:19 -0400

    go assignment finished, just need to double-check guidelines were followed and hand in.

---
+++ main.go
@@ -0,0 +1,87 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+	"unicode"
+)
+
+func main() {
+	// List of messages
+	messages := []string{"Csi2520", "Csi2120", "3 Paradigms",
+		"Go is 1st", "Prolog is 2nd", "Scheme is 3rd",
+		"uottawa.ca", "csi/elg/ceg/seg", "800 King Edward"}
+
+	done := make(chan int)
+	item := make(chan string)
+
+	caesarCipher("I love CS!", 5)
+	// PART 2 HERE, COMMENTED OUT CAUSE USED THE SAME CHANNELS AS PART 3
+	//go caesarCipherList(messages[:], 2, item, done)
+	fmt.Println()
+
+	go caesarCipherList(messages[:3], 2, item, done)
+	go caesarCipherList(messages[3:6], 2, item, done)
+	go caesarCipherList(messages[6:], 2, item, done)
+
+	sync := 0
+	for {
+		msg, _ := <-item
+		fmt.Println(msg)
+
+		if sync >= len(messages)-1 {
+			break
+		}
+		sync = sync + <-done
+	}
+	close(done)
+	close(item)
+}
+
+func caesarCipher(msg string, shift int) {
+	char := toRunes(msg)
+	var encode []rune
+
+	for _, i := range char {
+		if !(unicode.IsLetter(i)) {
+			continue
+		}
+		rn := int(i) + shift
+		if !(unicode.IsLetter(rune(rn))) {
+			rn -= 26
+		}
+		encode = append(encode, rune(rn))
+	}
+
+	fmt.Println(strings.ToUpper(string(encode)))
+}
+
+func caesarCipherList(msg []string, shift int, item chan string, done chan int) {
+	for _, m := range msg {
+		char := toRunes(m)
+		var encode []rune
+
+		for _, i := range char {
+			if !(unicode.IsLetter(i)) {
+				continue
+			}
+			rn := int(i) + shift
+			if !(unicode.IsLetter(rune(rn))) {
+				rn -= 26
+			}
+			encode = append(encode, rune(rn))
+		}
+		item <- strings.ToUpper(string(encode))
+		done <- 1
+	}
+}
+
+func toRunes(s string) []rune {
+	var r []rune // slice of unicode chars
+	// from string to slice of unicode
+	for _, c := range s {
+		r = append(r, c)
+	}
+
+	return r
+}
